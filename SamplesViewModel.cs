816bb7a433719db6b2aa4f8dafd2fc175f64b7f1
Amber Mills <amberdmills@live.com>
2024-08-04 16:16:22 -0500

    Add project files.

---
+++ SamplesViewModel.cs
@@ -0,0 +1,136 @@
+﻿namespace LINQSamples
+{
+  public class SamplesViewModel : ViewModelBase
+  {
+    #region OrderByQuery
+    /// <summary>
+    /// Order products by Name
+    /// </summary>
+    public List<Product> OrderByQuery()
+    {
+      List<Product> products = GetProducts();
+      List<Product> list = new();
+
+      // Write Query Syntax Here
+      list = (from product in products
+              orderby product.Name
+              select product).ToList();
+
+      return list;
+    }
+    #endregion
+
+    #region OrderByMethod
+    /// <summary>
+    /// Order products by Name
+    /// </summary>
+    public List<Product> OrderByMethod()
+    {
+      List<Product> products = GetProducts();
+      List<Product> list = new();
+
+      // Write Method Syntax Here
+     list = products
+         .OrderBy(product => product.Name)
+         .ToList();
+
+      return list;
+    }
+    #endregion
+
+    #region OrderByDescendingQuery Method
+    /// <summary>
+    /// Order products by name in descending order
+    /// </summary>
+    public List<Product> OrderByDescendingQuery()
+    {
+            List<Product> products = GetProducts();
+            List<Product> list = new();
+
+            // Write Query Syntax Here
+            list = (from product in products
+                    orderby product.Name descending
+                    select product).ToList();
+
+            return list;
+        }
+    #endregion
+
+    #region OrderByDescendingMethod Method
+    /// <summary>
+    /// Order products by name in descending order
+    /// </summary>
+    public List<Product> OrderByDescendingMethod()
+    {
+      List<Product> products = GetProducts();
+      List<Product> list = new();
+
+            // Write Method Syntax Here
+            list = products
+                .OrderByDescending(product => product.Name)
+                .ToList();
+
+
+            return list;
+    }
+    #endregion
+
+    #region OrderByTwoFieldsQuery Method
+    /// <summary>
+    /// Order products by Color descending, then Name
+    /// </summary>
+    public List<Product> OrderByTwoFieldsQuery()
+    {
+      List<Product> products = GetProducts();
+
+      var list =
+          // Write Query Syntax Here
+          (from product in products
+          orderby product.Color descending, product.Name
+          select product).ToList();
+
+            return list;
+    }
+    #endregion
+
+    #region OrderByTwoFieldsMethod Method
+    /// <summary>
+    /// Order products by Color descending, then Name
+    /// </summary>
+    public List<Product> OrderByTwoFieldsMethod()
+    {
+      List<Product> products = GetProducts();
+
+      var list =
+          // Write Method Syntax Here
+          products
+          .OrderBy(product => product.Color)
+          .ThenBy(product => product.Name)
+          .ToList();
+
+      return list;
+    }
+    #endregion
+
+    #region OrderByTwoFieldsDescMethod Method
+    /// <summary>
+    /// Order products by Color descending, then Name Descending
+    /// </summary>
+    public List<Product> OrderByTwoFieldsDescMethod()
+    {
+      List<Product> products = GetProducts();
+
+      var list =
+          // Write Method Syntax Here
+          products
+          .OrderByDescending(product => product.Color)
+          .ThenBy(product => product.Name)
+          .ToList();
+
+
+            return list;
+    }
+    #endregion
+
+  }
+}
